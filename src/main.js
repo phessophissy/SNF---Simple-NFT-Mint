876bc88d3514d566f218bbb143f7eaf4ff504d25
phessophissy <adefesobi@yahoo.com>
2026-04-23 23:54:13 +0100

    refactor: update main.js with note 4.4

--- src/main.js
+++ src/main.js
@@ -1285,3 +1285,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 /** Refactor Note 4.2: Improved internal logic for better performance. */
 
 /** Refactor Note 4.3: Improved internal logic for better performance. */
+
+/** Refactor Note 4.4: Improved internal logic for better performance. */
